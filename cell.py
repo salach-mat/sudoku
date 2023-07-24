7aa8759463409b98c488193d27e21079ea67522c
Salach <mateusz.salach@capgemini.com>
2023-07-24 10:47:35 +0200

    First version of solution

---
+++ cell.py
@@ -0,0 +1,6 @@
+class Cell:
+
+    def __init__(self, square, val):
+        self.square = square
+        self.cell_val = val
+
---
+++ main.py
@@ -0,0 +1,7 @@
+from board import Board
+
+
+
+if __name__ == '__main__':
+    b = Board()
+    b.draw_board()
